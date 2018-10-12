69bb530c004c983e3e8a67e3230325768dc823be
somalalimi15 <43244771+somalalimi15@users.noreply.github.com>
2018-10-12 15:46:56 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ var prefix = "H"
 
 client.on('message', message => {
    if(!message.channel.guild) return;
-if(message.content.startsWith(prefix = "$")) {
+if(message.content.startsWith(prefix = "H")) {
 if(!message.channel.guild) return message.channel.send('**هذا الأمر فقط للسيرفرات**').then(m => m.delete(5000));
 if(!message.member.hasPermission('ADMINISTRATOR')) return      message.channel.send('**للأسف لا تمتلك صلاحية** `ADMINISTRATOR`' );
 let args = message.content.split(" ").join(" ").slice(2 + prefix.length);
